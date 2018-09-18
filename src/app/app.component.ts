9d245bca1f26b921add3b4ad0adf0843285aa065
Jaspreet <jaspreetsingh.gulati@pearson.com>
2018-09-18 15:34:37 +0530

    Third Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy-app';
+  isText = false;
+  textLabel = 'Display Details';
+  timeArray:Array<Date> = [];
   resetName = function(){
     this.userName = '';
+  }
+
+  hideText = function(){
+    this.isText = !this.isText;
+    this.timeArray.push(Date.now());
+    console.log(this.timeArray);
+    if(this.isText){
+      this.textLabel = 'Hide Details'
+    }else{
+      this.textLabel = 'Display Details';
+    }
   }
 }
